e1088774600b835c6fb3a65f00e327ea4818074f
ok <ok@MacBook-Pro.local>
2024-10-10 09:11:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
-  
+def getInfo():
+    pod_name = os.getenv('POD_NAME', 'Unknown Pod')
+    node_name = os.getenv('NODE_NAME', 'Unknown Node')
+    pod_namespace = os.getenv('POD_NAMESPACE', 'Unknown Namespace')
+    
+    return f'Pod Name: {pod_name}, Node Name: {node_name}, Pod Namespace: {pod_namespace}'
+
 if __name__ == '__main__':
     app.run(host= '0.0.0.0',port=8080,debug=True)
