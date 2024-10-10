4c22aef7595f971558565404b60fa0d2794eaa6b
ok <ok@MacBook-Pro.local>
2024-10-10 09:16:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
+pod_name = os.getenv('POD_NAME', 'Unknown Pod')
+node_name = os.getenv('NODE_NAME', 'Unknown Node')
+pod_namespace = os.getenv('POD_NAMESPACE', 'Unknown Namespace')
+
 @app.route("/")
-def getInfo():
-    pod_name = os.getenv('POD_NAME', 'Unknown Pod')
-    node_name = os.getenv('NODE_NAME', 'Unknown Node')
-    pod_namespace = os.getenv('POD_NAMESPACE', 'Unknown Namespace')
-    
-    return f'Pod Name: {pod_name}, Node Name: {node_name}, Pod Namespace: {pod_namespace}'
+def index():
+    return " Container EDU | POD Working : " + pod_name + " | nodename : " +  node_name +  " | namespace : " +  pod_namespace + "\n"
 
 if __name__ == '__main__':
-    app.run(host= '0.0.0.0',port=8080,debug=True)
+    app.run(host= '0.0.0.0',port=5000,debug=True)
